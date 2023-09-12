7deac0cbae797fad6bdd16c807c753e775fc41ed
Sachin Biradar <139945811+apbSachin@users.noreply.github.com>
2023-09-12 07:52:58 +0000

    .

--- mergingArray.js
+++ mergingArray.js
@@ -5,10 +5,18 @@
 const obj1=[{a:1,b:2}];
 const obj2=[{b:5,d:4}];
 
+/**first appoach */
+
+// const result=obj1.map((value,index)=>{
+//     const item=obj2[index];
+//     const merged={...value,...item}
+//     return merged;
+// })
+
+/**second approach */
 const result=obj1.map((value,index)=>{
-    const item=obj2[index];
-    const merged={...value,...item}
+    const item=obj2[index]; //inside the arrow func we retriev the obj2 using index this allow us to pair up the element from both arrar
+    const merged=Object.assign(value,item)
     return merged;
 })
-
 console.log(result);
